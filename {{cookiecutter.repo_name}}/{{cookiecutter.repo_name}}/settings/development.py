f463c9cbaa43392ca79a9f6308bbafcc070c9eed
Ratson <github@ratson.name>
2015-10-19 16:53:29 +0800

    Install django-extensions

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/development.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/development.py
@@ -4,3 +4,12 @@ from .base import *  # noqa
 DEBUG = env.bool('DJANGO_DEBUG', default=True)
 
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='CHANGEME!!!')
+
+INSTALLED_APPS += (
+    'django_extensions',
+    'debug_toolbar',
+)
+
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
